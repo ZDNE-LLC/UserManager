6cf4c57e97f314fd69caa8154984eadc88b3c093
Zach <zdann120@users.noreply.github.com>
2017-06-10 14:27:56 -0700

    add-views
    Merges #1

--- app/controllers/user_manager/users_controller.rb
+++ app/controllers/user_manager/users_controller.rb
@@ -3,7 +3,7 @@ require_dependency "user_manager/application_controller"
 module UserManager
   class UsersController < ApplicationController
     def index
-      @users = User.all
+      @users = UserManager::Engine.config.user_model.all
     end
   end
 end
---
+++ test/dummy/config/initializers/user_manager.rb
@@ -0,0 +1,6 @@
+UserManager::Engine.config.user_model = User # => NameError: uninitialized constant UserManager
+
+# ~> NameError
+# ~> uninitialized constant UserManager
+# ~>
+# ~> /var/folders/k3/q4dpz6p977q18bhrhq97gv6r0000gn/T/seeing_is_believing_temp_dir20170610-40138-1ajmmg9/program.rb:1:in `<main>'
